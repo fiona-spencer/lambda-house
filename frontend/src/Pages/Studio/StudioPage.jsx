196923e21a54c77ecb925de0405d7687e5679671
Fiona Spencer <fionagspencer@gmail.com>
2025-06-28 12:14:51 -0400

    2025-06-28 12:14:51 - update

--- frontend/src/Pages/Studio/StudioPage.jsx
+++ frontend/src/Pages/Studio/StudioPage.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import Viewer from './Upload/Viewer'  // adjust path if needed
-import React, { useRef } from 'react';
 import { Canvas, useFrame } from '@react-three/fiber';
 import { OrbitControls } from '@react-three/drei';
 
