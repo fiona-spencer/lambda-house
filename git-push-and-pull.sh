1c854b8589f6614ae82980b53f8222ae676b3fcc
Fiona Spencer <fionagspencer@gmail.com>
2025-06-28 00:22:54 -0400

    local update

---
+++ git-push-and-pull.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Push local changes to origin main
+git push origin main
+
+# SSH to server and pull latest changes
+ssh fiona_spencer 'cd /volume1/docker/lambda-house/frontend/src && git pull origin main'
