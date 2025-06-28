e0c73b1bf83c18385fad939c12162d0386eb79c8
Fiona Spencer <fionagspencer@gmail.com>
2025-06-28 12:30:25 -0400

    remove three js

--- frontend/src/Pages/Studio/Upload/Viewer.jsx
+++ frontend/src/Pages/Studio/Upload/Viewer.jsx
@@ -1,35 +1,7 @@
-import React, { useRef } from 'react';
-import { Canvas, useFrame } from '@react-three/fiber';
-import { OrbitControls } from '@react-three/drei';
-
-function RotatingCube() {
-  const meshRef = useRef();
-
-  // Rotate the cube every frame
-  useFrame(() => {
-    if (meshRef.current) {
-      meshRef.current.rotation.x += 0.01;
-      meshRef.current.rotation.y += 0.01;
-    }
-  });
-
-  return (
-    <mesh ref={meshRef} position={[0, 0, 0]}>
-      <boxGeometry args={[2, 2, 2]} />
-      <meshStandardMaterial color="royalblue" />
-    </mesh>
-  );
-}
+import React from 'react'
 
 export default function Viewer() {
   return (
-    <div style={{ width: '100vw', height: '100vh' }}>
-      <Canvas camera={{ position: [5, 5, 5], fov: 60 }}>
-        <ambientLight intensity={0.4} />
-        <directionalLight position={[5, 5, 5]} intensity={0.8} />
-        <RotatingCube />
-        <OrbitControls />
-      </Canvas>
-    </div>
-  );
+    <div>Viewer</div>
+  )
 }
