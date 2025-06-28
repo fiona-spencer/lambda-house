60d6d338f77742bae1344bc3cad7905dd72c43e1
Fiona Spencer <fionagspencer@gmail.com>
2025-06-28 09:24:03 -0400

    2025-06-28 09:24:03 - test

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
-import Main from './Home/Main';
 import LambdaHouse from './Home/Lambdahouse';
 import './index.css';
 
@@ -8,14 +7,12 @@ function App() {
   return (
     <BrowserRouter>
       <nav className="bg-gray-100 p-4 flex space-x-6 shadow-md">
-        <Link to="/" className="text-blue-600 hover:tex-red-800 font-semibold">Home</Link>
         <Link to="/lambda-house" className="text-red-600 hover:text-blue-700 font-semibold">Lambda NAS House</Link>
         <Link to="/lambda-house" className="text-blue-600 hover:text-blue-800 font-semibold">Hello There</Link>
       </nav>
       <main className="p-6 max-w-5xl mx-auto">
         <Routes>
-          <Route path="/" element={<Main />} />
-          <Route path="/lambda-house" element={<LambdaHouse />} />
+          <Route path="/" element={<LambdaHouse />} />
         </Routes>
       </main>
     </BrowserRouter>
