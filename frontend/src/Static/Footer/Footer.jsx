2eb762c5eb65f0c0535a4a2137117cd4988f7af6
Fiona Spencer <fionagspencer@gmail.com>
2025-06-29 12:40:29 -0400

    2025-06-29 12:40:29

--- frontend/src/Static/Footer/Footer.jsx
+++ frontend/src/Static/Footer/Footer.jsx
@@ -4,7 +4,7 @@ import lgTransparent from '../../assets/lh-transparent.svg'
 
 function Footer() {
   return (
-    <footer className="relative overflow-hidden bg-opacity-60 bg-black py-12 text-center text-white shadow-lg mt-10">
+    <footer className="relative overflow-hidden bg-opacity-70 bg-black py-12 text-center text-white shadow-lg mt-10">
       {/* Content container */}
       <div className="relative z-10 max-w-7xl mx-auto px-2">
         {/* Logo */}
@@ -13,7 +13,7 @@ function Footer() {
           alt="Lambda House Logo" 
           className="h-32 w-auto object-contain"/>
         </div>
-        <div className="flex justify-center gap-14 mb-2 text-5xl">
+        <div className="flex justify-center gap-14 mb-10 text-5xl">
           <a
             href="https://www.youtube.com/@LambdaHouse416"
             target="_blank"
@@ -51,7 +51,7 @@ className="text-white hover:text-gray-300 transition duration-300 transform hove
         </div>
 
         {/* Policy Links */}
-        <div className="flex flex-wrap justify-center gap-8 mb-12 text-sm text-blue-200 font-light">
+        <div className="flex flex-wrap justify-center gap-8 mb-12 text-sm text-blue font-light">
           {[
             ["Terms and Conditions", "/terms-and-conditions"],
             ["Shipping Policy", "/shipping-policy"],
@@ -71,7 +71,7 @@ className="text-white hover:text-gray-300 transition duration-300 transform hove
         </div>
 
         {/* Copyright */}
-        <div className="text-xs text-blue-300 font-mono tracking-wide select-none">
+        <div className="text-xs text-white-300 font-mono tracking-wide select-none">
           &copy; {new Date().getFullYear()} Lambda House — All rights reserved.
         </div>
       </div>
