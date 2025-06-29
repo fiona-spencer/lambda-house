c488fb9475460f9ecf42d635752a7ed53875161f
Fiona Spencer <fionagspencer@gmail.com>
2025-06-29 14:00:03 -0400

    2025-06-29 14:00:03

--- frontend/src/Static/Header/Header.jsx
+++ frontend/src/Static/Header/Header.jsx
@@ -37,7 +37,7 @@ function Header() {
     </div>
 
     {/* User + Cart */}
-<div className="flex space-x-10 order-2 sm:order-3 w-auto ml-auto">
+<div className="flex space-x-4 order-2 sm:order-3 w-auto ml-auto">
   <Dropdown
     inline
     label={
