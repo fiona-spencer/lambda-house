4646b4c32393cb110d52ed75563f9f78dafb7f9d
Fiona Spencer <fionagspencer@gmail.com>
2025-06-29 15:16:54 -0400

    2025-06-29 15:16:54

--- frontend/src/Pages/Home/Sections.jsx
+++ frontend/src/Pages/Home/Sections.jsx
@@ -50,7 +50,7 @@ export default function Sections() {
             <h4 className="text-md font-medium text-gray-600 mb-2">{item.subtitle}</h4>
             <p className="text-sm text-gray-800 mb-4">{item.description}</p>
           </div>
-<Link to={item.link} className="mt-auto flex justify-center group">
+<Link to={item.link} className="mt-auto absolute bottom-6 justify-center group">
   <span className="px-10 py-2 rounded-full transition duration-300 group-hover:bg-pink-500/50">
     <FaArrowRight className="text-2xl transition-transform duration-300 group-hover:translate-x-1" />
   </span>
