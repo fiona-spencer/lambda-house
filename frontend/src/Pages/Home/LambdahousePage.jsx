6e9333c1c6f8f8bc6e6efe5d76e7ad92247fcb54
Fiona Spencer <fionagspencer@gmail.com>
2025-06-29 15:26:12 -0400

    2025-06-29 15:26:12

--- frontend/src/Pages/Home/LambdahousePage.jsx
+++ frontend/src/Pages/Home/LambdahousePage.jsx
@@ -1,12 +1,27 @@
-import React from 'react'
-import Sections from './Sections'
-import EmailContact from '../../Components/EmailContact'
+import React from 'react';
+import Sections from './Sections';
+import EmailContact from '../../Components/EmailContact';
+
+const sections = [
+  {
+    title: "OUR SERVICES",
+    component: Sections,
+  },
+  {
+    title: "CONTACT",
+    component: EmailContact,
+  },
+];
 
 export default function LambdahousePage() {
   return (
-    <div>
-      <Sections/>
-      <EmailContact/>
+    <div className="space-y-12 px-6">
+      {sections.map(({ title, component: Component }, idx) => (
+        <div key={idx}>
+          <div className="text-4xl font-bold text-black mb-6">{title}</div>
+          <Component />
+        </div>
+      ))}
     </div>
-  )
+  );
 }
