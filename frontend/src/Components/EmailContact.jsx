02cd6310ea481c74a386fb54e6da208b2b2fda72
Fiona Spencer <fionagspencer@gmail.com>
2025-06-29 15:21:52 -0400

    2025-06-29 15:21:52

---
+++ frontend/src/Components/EmailContact.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function EmailContact() {
+  return (
+    <div>EmailContact</div>
+  )
+}
--- frontend/src/Pages/Home/LambdahousePage.jsx
+++ frontend/src/Pages/Home/LambdahousePage.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Sections from './Sections'
+import EmailContact from '../../Components/EmailContact'
 
 export default function LambdahousePage() {
   return (
     <div>
       <Sections/>
+      <EmailContact/>
     </div>
   )
 }
