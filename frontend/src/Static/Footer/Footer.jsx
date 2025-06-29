4f483e63715588320f4caf2ae27c26dc08ba4d14
Fiona Spencer <fionagspencer@gmail.com>
2025-06-29 13:41:45 -0400

    2025-06-29 13:41:45

--- frontend/src/Static/Footer/Footer.jsx
+++ frontend/src/Static/Footer/Footer.jsx
@@ -53,7 +53,7 @@ className="text-white hover:text-gray-300 transition duration-300 transform hove
         </div>
 
         {/* Policy Links */}
-        <div className="sm:flex sm:flex-wrap col-span-1 justify-center gap-8 text-sm text-white bg-black p-4">
+        <div className="sm:flex sm:flex-wrap row-span-1 justify-center gap-8 text-sm text-white bg-black p-4">
           {[
             ["Terms and Conditions", "/terms-and-conditions"],
             ["Shipping Policy", "/shipping-policy"],
