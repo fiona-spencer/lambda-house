3fcdd8c8d1b856c03fd829c87ee75948681ce410
Fiona Spencer <fionagspencer@gmail.com>
2025-06-29 20:14:49 -0400

    2025-06-29 20:14:49

--- frontend/src/Pages/Home/HeroText.jsx
+++ frontend/src/Pages/Home/HeroText.jsx
@@ -2,8 +2,10 @@ import React from 'react'
 
 export default function HeroText() {
   return (
-    <div className="text-3xl leading-relaxed py-4 bg-pink-500">
-We are a Toronto-based 3D printing studio offering high-quality prints, custom model design, and integrated electronics solutions for makers, artists, and businesses 
+    <div className="text-3xl leading-relaxed py-4 bg-pink-500 text-white font-semibold">
+      <div className="px-4">
+        We are a Toronto-based 3D printing studio offering high-quality prints, custom model design, and integrated electronics solutions for makers, artists, and businesses 
+      </div>
    </div>
   )
 }
