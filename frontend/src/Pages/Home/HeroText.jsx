9e82c83b4bc60e7e386793b31532f28516052eb5
Fiona Spencer <fionagspencer@gmail.com>
2025-06-29 20:04:59 -0400

    2025-06-29 20:04:59

--- frontend/src/Pages/Home/HeroText.jsx
+++ frontend/src/Pages/Home/HeroText.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 
 export default function HeroText() {
   return (
-    <div>HeroText</div>
+    <div className='-pt-7'>
+      We specialize in high-quality 3D printing services, custom model design, and advanced engineering solutions, including electronics integration. Whether you're an artist, maker, or business, we bring your ideas to life with precision and innovation.
+    </div>
   )
 }
