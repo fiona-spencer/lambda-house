b8a69ea0d89ec6b18c74aa3634d8cda9e2dbabe6
Fiona Spencer <fionagspencer@gmail.com>
2025-06-30 15:42:57 -0400

    2025-06-30 15:42:57

--- frontend/src/Pages/Home/LambdahousePage.jsx
+++ frontend/src/Pages/Home/LambdahousePage.jsx
@@ -45,7 +45,7 @@ export default function LambdahousePage() {
       ))}
       {/* Responsive layout for House components */}
     <div className="flex flex-col sm:flex-row justify-center sm:gap-6 items-center space-y-6 sm:space-y-0">
-  <House />
+  <House className="sm:block"/>
   <div className="hidden sm:block">
     <House />
   </div>
