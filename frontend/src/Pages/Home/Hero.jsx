7765e9abf80c17506366017fc61a301fda2edd3e
Fiona Spencer <fionagspencer@gmail.com>
2025-06-29 20:19:12 -0400

    2025-06-29 20:19:12

--- frontend/src/Pages/Home/Hero.jsx
+++ frontend/src/Pages/Home/Hero.jsx
@@ -8,7 +8,7 @@ export default function Hero() {
   };
 
   return (
-    <div className="flex w-full sm:h-[calc(90vh)] h-[calc(93vh)] overflow-hidden">
+    <div className="flex w-full sm:h-[calc(82vh)] h-[calc(80vh)] overflow-hidden">
       {/* Top Small Text */}
       <div className="absolute top-4 left-1/2 transform -translate-x-1/2 z-20 text-white text-sm tracking-widest uppercase">
         Welcome to Lambda House
