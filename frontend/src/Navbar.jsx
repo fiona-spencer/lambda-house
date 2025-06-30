dc902039b7ef49e9dc8ed26d8e2ebdd1b33a9be0
Fiona Spencer <fionagspencer@gmail.com>
2025-06-29 20:54:33 -0400

    2025-06-29 20:54:33

--- frontend/src/Navbar.jsx
+++ frontend/src/Navbar.jsx
@@ -48,35 +48,44 @@ const navItems = [
 
 export default function NavBar() {
   return (
-    <nav className="bg-pink-500 p-4 flex justify-center space-x-8 shadow-md relative">
-      {navItems.map(({ page, link, subpages }) => (
-        <div key={page} className="relative group">
-          <Link
-            to={link}
-            className="text-gray-800 font-semibold hover:text-gray-600"
-          >
-            {page}
-          </Link>
+<nav className="bg-pink-500 p-4 flex justify-center space-x-8 shadow-md relative">
+  {navItems.map(({ page, link, subpages }) => (
+    <div key={page} className="relative group">
+      <Link
+        to={link}
+        className="text-gray-800 font-semibold hover:text-gray-600"
+      >
+        {page}
+      </Link>
 
-          {/* Dropdown menu */}
-          {subpages && subpages.length > 0 && (
-            <div className="absolute left-1/2 transform -translate-x-1/2 mt-2 w-48 bg-white border border-gray-300 rounded shadow-lg opacity-0 invisible group-hover:opacity-100 group-hover:visible transition-opacity duration-200 z-50">
-              <ul>
-                {subpages.map(({ title, link: subLink }) => (
-                  <li key={title}>
-                    <Link
-                      to={subLink}
-                      className="block px-4 py-2 text-gray-700 hover:bg-pink-500 hover:text-white"
-                    >
-                      {title}
-                    </Link>
-                  </li>
-                ))}
-              </ul>
-            </div>
-          )}
+      {/* Dropdown menu */}
+      {subpages && subpages.length > 0 && (
+        <div
+          className="absolute left-1/2 transform -translate-x-1/2 mt-2 w-48 bg-white border border-gray-300 rounded shadow-lg
+          opacity-0 invisible
+          group-hover:opacity-100 group-hover:visible
+          transition-opacity duration-200 z-50"
+          // Add pointer events so dropdown can be interacted with
+          onMouseEnter={e => e.currentTarget.style.visibility = 'visible'}
+          onMouseLeave={e => e.currentTarget.style.visibility = 'hidden'}
+        >
+          <ul>
+            {subpages.map(({ title, link: subLink }) => (
+              <li key={title}>
+                <Link
+                  to={subLink}
+                  className="block px-4 py-2 text-gray-700 hover:bg-pink-500 hover:text-white"
+                >
+                  {title}
+                </Link>
+              </li>
+            ))}
+          </ul>
         </div>
-      ))}
-    </nav>
+      )}
+    </div>
+  ))}
+</nav>
+
   );
 }
