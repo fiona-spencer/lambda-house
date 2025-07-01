2366d30d6abfea3f432343bdfaa313f3e288a994
Fiona Spencer <fionagspencer@gmail.com>
2025-06-30 22:32:07 -0400

    2025-06-30 22:32:07

--- backend/index.js
+++ backend/index.js
@@ -31,6 +31,6 @@ app.get('*', (req, res) => {
 
 // Server Start
 const PORT = process.env.PORT || 8000;
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`🚀 Server running on port ${PORT}`);
 });
