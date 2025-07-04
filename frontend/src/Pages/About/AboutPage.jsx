2457f6b27a5b23927b9ca738d62107a8ba982113
Fiona Spencer <fionagspencer@gmail.com>
2025-07-04 14:19:33 -0400

    2025-07-04 14:19:32

--- frontend/src/Pages/About/AboutPage.jsx
+++ frontend/src/Pages/About/AboutPage.jsx
@@ -5,7 +5,12 @@ export default function AboutPage() {
   return (
     <main className="min-h-screen text-gray-900 flex flex-col items-center justify-center p-8 space-y-12 bg-white relative">
       
-
+      {/* Top-left image */}
+<img
+  src={smallLogo}
+  alt="Lambda House Logo"
+  className="w-40 h-40 mx-auto rainbow object-contain mb-8"
+/>
 
       {/* Intro Section */}
       <section className="max-w-4xl w-full bg-gray-200 p-8">
@@ -65,12 +70,6 @@ export default function AboutPage() {
             lambdahouse416@gmail.com
           </a>
         </p>
-              {/* Top-left image */}
-<img
-  src={smallLogo}
-  alt="Lambda House Logo"
-  className="w-40 h-40 mx-auto rainbow object-contain mb-8"
-/>
       </section>
     </main>
   );
