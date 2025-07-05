bcf8a854ff4ccb407a28d2c6f459d69112983d9a
Fiona Spencer <fionagspencer@gmail.com>
2025-07-04 23:53:33 -0400

    2025-07-04 23:53:32

--- frontend/src/Pages/Log/LogsPage.jsx
+++ frontend/src/Pages/Log/LogsPage.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import marked from "marked";
+import ReactMarkdown from "react-markdown";
 
 export default function LogsPage() {
   const [markdownContent, setMarkdownContent] = useState("");
@@ -12,8 +12,7 @@ export default function LogsPage() {
     )
       .then((res) => res.text())
       .then((text) => {
-        const html = marked.parse(text);
-        setMarkdownContent(html);
+        setMarkdownContent(text);
         setLoading(false);
       })
       .catch((err) => {
@@ -25,9 +24,7 @@ export default function LogsPage() {
   return (
     <main className="p-8 prose max-w-none text-black bg-white border border-gray-300">
       {loading && <p>Loading...</p>}
-      {!loading && (
-        <article dangerouslySetInnerHTML={{ __html: markdownContent }} />
-      )}
+      {!loading && <ReactMarkdown>{markdownContent}</ReactMarkdown>}
     </main>
   );
 }
