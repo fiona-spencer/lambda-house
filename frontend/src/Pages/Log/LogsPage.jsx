46caefaabaebb9ba73cc0704e80485a7043c9b70
Fiona Spencer <fionagspencer@gmail.com>
2025-07-05 12:31:42 -0400

    2025-07-05 12:31:42

--- frontend/src/Pages/Log/LogsPage.jsx
+++ frontend/src/Pages/Log/LogsPage.jsx
@@ -106,7 +106,7 @@ export default function LogsPage() {
     <div className="relative flex flex-col sm:flex-row min-h-screen bg-white text-black">
       {/* Toggle Tab (Right side) - Small screens only */}
       <button
-        className="sm:hidden fixed top-[400px] right-0 transform rotate-90 bg-pink-600 text-white px-3 -mr-4 py-1 rounded-b-md z-30 shadow-md"
+        className="sm:hidden fixed top-[500px] right-0 transform rotate-90 bg-pink-600 text-white px-3 -mr-4 py-1 rounded-b-md z-30 shadow-md"
         onClick={() => setSidebarOpen(!sidebarOpen)}
       >
         Logs
